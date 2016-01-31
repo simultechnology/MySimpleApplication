6138d360ed9aaf48a2ad2dbc65a2b7153d43de83
t_ishikawa <tissi0708@gmail.com>
2016-02-01 07:59:42 +0900

    Initial commit

---
+++ app/src/main/java/com/simultechnology/mysimpleapplication/MainActivity.java
@@ -0,0 +1,121 @@
+package com.simultechnology.mysimpleapplication;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LocationManager mLocationManager;
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        boolean gpsFlg = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        Log.d("NETWORK Enabled", gpsFlg ? "OK" : "NG");
+        onBtnGpsClicked();
+
+        Button gpsBtn = (Button) findViewById(R.id.gps_btn);
+
+        gpsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBtnGpsClicked();
+            }
+        });
+
+        mWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.loadUrl("https://sample-simultechnology.c9users.io/map.html");
+        //myWebView.loadUrl("http://www.yahoo.co.jp");
+
+    }
+
+    // GPSボタン
+    public void onBtnGpsClicked() {
+        Log.d("GPS", "start!");
+        mLocationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER, //LocationManager.NETWORK_PROVIDER,
+                0, // 通知のための最小時間間隔（ミリ秒）
+                0, // 通知のための最小距離間隔（メートル）
+                new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        String msg = "Lat=" + location.getLatitude()
+                                + "\nLng=" + location.getLongitude();
+                        Log.d("GPS", msg);
+                        TextView view = (TextView) findViewById(R.id.latlon_area);
+                        view.setText(msg);
+                        mWebView.evaluateJavascript("alert('" + location.getLatitude() + "," + location.getLongitude() + "');", null);
+                        mLocationManager.removeUpdates(this);
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
